1aa7b3bf09c6573eec89027b4b8d33402e77b578
Kieran Campbell <kieranrcampbell@gmail.com>
2014-12-16 12:06:43 +0000

    Filled out package structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+
+setup(name='curver',
+	version=0.1,
+	description='Curve reconstruction from noisy points',
+	author='Kieran Campbell',
+	author_email='kieranrcampbell@gmail.com',
+	license='Apache 2.0')
